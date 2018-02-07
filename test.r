d4356de5ed7dda80916afe26f2e13e345d5a9b35
Fabian Leisibach <fabian.leisibach@gmail.comgit config --global user.name KoenigFabian>
2018-02-07 10:22:05 +0100

    merge conflict resolved

--- test.r
+++ test.r
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 setwd("/Users/fabianleisibach/Desktop")
 myData <- fread("transactions.csv")
-=======
-myData <- fread("/Users/fabianleisibach/Cloud/UZH/Semester 7/R – A non-technical introduction to big data techniques, team work and interactive visualization/data/transactions.csv")
->>>>>>> parent of acf0727... added working directory
 str(myData)
 myData[, TransDate:=dmy(TransDate, tz="UTC")]
 str(myData)
